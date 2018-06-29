67b85b48dea3472eef86762378edeb778e07e0b9
Dilip Kumar Vedagiri <dvedagiri@BLR.ALLEGISINDIA.COM>
2018-06-29 13:22:56 +0530

    Maven Project First Commit

---
+++ src/test/java/MavenProject/MavenProject/TestClass.java
@@ -0,0 +1,28 @@
+package MavenProject.MavenProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Test 1");
+	}
+	@Test
+	public void testNew()
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
+
+}
